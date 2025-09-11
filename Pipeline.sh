40c688675f36a8c2428b9d853393704c6242de9c
Thorsten Kaltenbacher <thorsten.kaltenbacher@gmail.com>
2025-09-11 18:31:30 -0400

    Corrected some wrong variable naming and added some code annotation for better understanding

--- Pipeline.sh
+++ Pipeline.sh
@@ -56,13 +56,15 @@ done
 ## Run Binddetect for single condition or differential binding
 
 # Generate Peak Header file before running BINDetect
-awk 'BEGIN{OFS="\t"} {print $1, $2, $3, $4}' "${REGIONS}" > "${OUTDIR}/BINDetect/Header_ATAC_Peaks.txt"
+awk 'BEGIN{OFS="\t"} {print $1, $2, $3, $4}' "${regions_file}" > "${OUTDIR}/BINDetect/Header_ATAC_Peaks.txt"
 
 # Check if user chose Single or Differential Analysis
 if [[ "$MODUS" == "Single" ]]; then
     # Loop over all footprint bigWig files
     for bw_file in "${OUTDIR}/Footprint_Scores/"*_footprints_score.bw; do
-        # Extract sample name from filename
+        # Extract sample name from filename (strip path and suffix (replacement with nothing through sed command)).
+        # This identifier will be used for folder structure and tracking.
+
         SAMPLE=$(basename "$bw_file" | sed 's/_footprints_score.bw//')
 
         echo "Running BINDetect for single sample: $SAMPLE"
@@ -79,23 +81,23 @@ if [[ "$MODUS" == "Single" ]]; then
 
 #if user chose Differential analysis
 elif [[ "$MODUS" == "Differential" ]]; then
-    # Make sure at least two samples exist
-    if [[ -z "$SAMPLE_A" || -z "$SAMPLE_B" -lt 2 ]]; then
-        echo "Error: For Differential mode, please provide SAMPLE_A and SAMPLE_B as arguments."
+    # Make sure CONTROL and TREATMENT Sample were provided
+    if [[ -z "$CONTROL" || -z "$TREATMENT" ]]; then
+        echo "Error: For Differential mode, please provide CONTROL and TREATMENT as arguments."
         exit 1
     fi
 
-    echo "Running BINDetect for differential analysis: $SAMPLE_A vs $SAMPLE_B"
+    echo "Running BINDetect for differential analysis: $CONTROL vs $TREATMENT"
 
     TOBIAS BINDetect \
         --motifs "${MOTIFS}" \
-        --signals "${OUTDIR}/Footprint_Scores/${SAMPLE_A}_footprints_score.bw" \
-                  "${OUTDIR}/Footprint_Scores/${SAMPLE_B}_footprints_score.bw" \
+        --signals "${OUTDIR}/Footprint_Scores/${CONTROL}_footprints_score.bw" \
+                  "${OUTDIR}/Footprint_Scores/${TREATMENT}_footprints_score.bw" \
         --genome "${GENOME}" \
         --peaks "${regions_file}" \
         --peak_header "${OUTDIR}/BINDetect/Header_ATAC_Peaks.txt" \
-        --outdir "${OUTDIR}/BINDetect/${SAMPLE_A}_vs_${SAMPLE_B}" \
-        --cond_names "${SAMPLE_A}" "${SAMPLE_B}"
+        --outdir "${OUTDIR}/BINDetect/${CONTROL}_vs_${TREATMENT}" \
+        --cond_names "${CONTROL}" "${TREATMENT}"
 
 else
     echo "Error: MODUS not given. Choose between 'Single' or 'Differential'."
@@ -103,82 +105,82 @@ else
 fi
 
 
-#Plottracks
-    # ============================== CONFIG ==============================
-REGIONS="/media/rad/HDD1/2023_AnalysisTK/2023_ACC_ATAC/Nextflow_Results_D_A_Comp/bwa/mergedReplicate/2025_Footprinting/ATAC_peaks_overlapping_HiC_TSS_only_whole_promoter.bed"
-GTF="gtf_no_chr.gtf"
+# #Plottracks
+#     # ============================== CONFIG ==============================
+# REGIONS="/media/rad/HDD1/2023_AnalysisTK/2023_ACC_ATAC/Nextflow_Results_D_A_Comp/bwa/mergedReplicate/2025_Footprinting/ATAC_peaks_overlapping_HiC_TSS_only_whole_promoter.bed"
+# GTF="gtf_no_chr.gtf"
 
-# BigWig files
-BIGWIGS=(
-    "/media/rad/HDD1/ChipSeqThorstenWS6/TK_2022_Foxp1_pvalue005_consensus3/bwa/mergedLibrary/bigwig/PKF1OE_R4.bigWig"
-    "/media/rad/HDD1/2023_AnalysisTK/2023_ACC_ATAC/Nextflow_Results_D_A_Comp/bwa/mergedReplicate/bigwig/GFP0h.mRp.clN.bigWig"
-    "/media/rad/HDD1/2023_AnalysisTK/2023_ACC_ATAC/Nextflow_Results_D_A_Comp/bwa/mergedReplicate/2025_Footprinting/GFP0h.mRp.clN.sorted_corrected.bw"
-    "/media/rad/HDD1/2023_AnalysisTK/2023_ACC_ATAC/Nextflow_Results_D_A_Comp/bwa/mergedReplicate/bigwig/KrFo24h.mRp.clN.bigWig"
-    "/media/rad/HDD1/2023_AnalysisTK/2023_ACC_ATAC/Nextflow_Results_D_A_Comp/bwa/mergedReplicate/2025_Footprinting/KrFo24h.mRp.clN.sorted_corrected.bw"
-)
+# # BigWig files
+# BIGWIGS=(
+#     "/media/rad/HDD1/ChipSeqThorstenWS6/TK_2022_Foxp1_pvalue005_consensus3/bwa/mergedLibrary/bigwig/PKF1OE_R4.bigWig"
+#     "/media/rad/HDD1/2023_AnalysisTK/2023_ACC_ATAC/Nextflow_Results_D_A_Comp/bwa/mergedReplicate/bigwig/GFP0h.mRp.clN.bigWig"
+#     "/media/rad/HDD1/2023_AnalysisTK/2023_ACC_ATAC/Nextflow_Results_D_A_Comp/bwa/mergedReplicate/2025_Footprinting/GFP0h.mRp.clN.sorted_corrected.bw"
+#     "/media/rad/HDD1/2023_AnalysisTK/2023_ACC_ATAC/Nextflow_Results_D_A_Comp/bwa/mergedReplicate/bigwig/KrFo24h.mRp.clN.bigWig"
+#     "/media/rad/HDD1/2023_AnalysisTK/2023_ACC_ATAC/Nextflow_Results_D_A_Comp/bwa/mergedReplicate/2025_Footprinting/KrFo24h.mRp.clN.sorted_corrected.bw"
+# )
 
-LABELS=("Chip" "GFPATAC" "GFPFoot" "PKRFATAC" "PKRFFoot")
+# LABELS=("Chip" "GFPATAC" "GFPFoot" "PKRFATAC" "PKRFFoot")
 
-COLORS=(
-    "green"
-    "orange"
-    "blue"
-    "orange"
-    "blue"
-)
+# COLORS=(
+#     "green"
+#     "orange"
+#     "blue"
+#     "orange"
+#     "blue"
+# )
 
-# Validate
-if [[ ${#BIGWIGS[@]} -ne ${#LABELS[@]} ]] || [[ ${#BIGWIGS[@]} -ne ${#COLORS[@]} ]]; then
-    echo "ERROR: Mismatch between number of bigwigs, labels, and colors"
-    exit 1
-fi
+# # Validate
+# if [[ ${#BIGWIGS[@]} -ne ${#LABELS[@]} ]] || [[ ${#BIGWIGS[@]} -ne ${#COLORS[@]} ]]; then
+#     echo "ERROR: Mismatch between number of bigwigs, labels, and colors"
+#     exit 1
+# fi
 
-# ============================== MAIN LOOP ==============================
-Conditions=("GFP0h" "KrFo24h")
+# # ============================== MAIN LOOP ==============================
+# Conditions=("GFP0h" "KrFo24h")
 
-for Sample in "${Conditions[@]}"; do
-    TFBS="merged_selected_TFs_list_${Sample}_bound_unique.bed"
+# for Sample in "${Conditions[@]}"; do
+#     TFBS="merged_selected_TFs_list_${Sample}_bound_unique.bed"
 
-    if [[ ! -f "$TFBS" ]]; then
-        echo "?? Warning: TFBS file not found for $Sample: $TFBS"
-        continue
-    fi
+#     if [[ ! -f "$TFBS" ]]; then
+#         echo "?? Warning: TFBS file not found for $Sample: $TFBS"
+#         continue
+#     fi
 
-    OUTDIR="TOBIAS_latest_samplespecific_final/${Sample}"
-    mkdir -p "$OUTDIR"
+#     OUTDIR="TOBIAS_latest_samplespecific_final/${Sample}"
+#     mkdir -p "$OUTDIR"
 
-    echo "Running TOBIAS PlotTracks for $Sample..."
+#     echo "Running TOBIAS PlotTracks for $Sample..."
 
-    TOBIAS PlotTracks \
-        --bigwigs "${BIGWIGS[@]}" \
-        --regions "$REGIONS" \
-        --sites "$TFBS" \
-        --gtf "$GTF" \
-        --labels "${LABELS[@]}" \
-        --colors "${COLORS[@]}" \
-        --width 15 \
-        --max-transcripts 1 \
-        --outdir "$OUTDIR"
+#     TOBIAS PlotTracks \
+#         --bigwigs "${BIGWIGS[@]}" \
+#         --regions "$REGIONS" \
+#         --sites "$TFBS" \
+#         --gtf "$GTF" \
+#         --labels "${LABELS[@]}" \
+#         --colors "${COLORS[@]}" \
+#         --width 15 \
+#         --max-transcripts 1 \
+#         --outdir "$OUTDIR"
 
-    echo "PlotTracks completed for $Sample ? $OUTDIR"
-done
+#     echo "PlotTracks completed for $Sample ? $OUTDIR"
+# done
 
-### GTF Filtering
+# ### GTF Filtering
 
-#!/bin/bash
+# #!/bin/bash
 
-# ========= CONFIGURE THIS =========
-INPUT_GTF="gencode.vM10.annotation.gtf.gz"                     # Path to compressed GTF
-GENE_LIST=("Foxp1" "Frmd4b" "Lmod3" )                                    # Genes to keep
-OUTPUT_GTF="filtered_TF_genes.gtf"                            # Output path
-# ==================================
+# # ========= CONFIGURE THIS =========
+# INPUT_GTF="gencode.vM10.annotation.gtf.gz"                     # Path to compressed GTF
+# GENE_LIST=("Foxp1" "Frmd4b" "Lmod3" )                                    # Genes to keep
+# OUTPUT_GTF="filtered_TF_genes.gtf"                            # Output path
+# # ==================================
 
-# Create a regex pattern like: (IRF1|IRF2|...)
-PATTERN=$(IFS=\| ; echo "${GENE_LIST[*]}")
+# # Create a regex pattern like: (IRF1|IRF2|...)
+# PATTERN=$(IFS=\| ; echo "${GENE_LIST[*]}")
 
-# Decompress and filter
-zcat "$INPUT_GTF" | awk -v pat="gene_name \"(${PATTERN})\"" '
-    $0 ~ pat { print }
-' > "$OUTPUT_GTF"
+# # Decompress and filter
+# zcat "$INPUT_GTF" | awk -v pat="gene_name \"(${PATTERN})\"" '
+#     $0 ~ pat { print }
+# ' > "$OUTPUT_GTF"
 
-echo "Filtered GTF written to: $OUTPUT_GTF"
+# echo "Filtered GTF written to: $OUTPUT_GTF"
